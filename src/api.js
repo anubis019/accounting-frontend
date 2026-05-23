c24a05b692a8cbb54c7e48061aa7c421b6972f11
anubis019 <165398929+anubis019@users.noreply.github.com>
2026-05-23 13:32:58 +0300

    update api.js

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 export const API_HOST =
   import.meta.env.VITE_API_URL ||
-  'https://accounting-backend-production-e5e6.up.railway.app'
+  'https://accounting-backend-production-2ded.up.railway.app/api'
 
 const api = axios.create({
   baseURL: `${API_HOST}/api`,
